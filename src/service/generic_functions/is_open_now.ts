2285cd453b1a417bb81e99bd6cddca2795b9e2ee
renan mascarenhas goncalves <renanmascg@gmail.com>
2020-07-21 01:15:58 -0300

    Cria serviço que calcula se o estabelecimento está aberto ou fechado no momento.

---
+++ src/service/generic_functions/is_open_now.ts
@@ -0,0 +1,49 @@
+import moment from 'moment';
+import { ServiceInterface } from '../../models/interfaces/service_interface';
+import { DE_PARA_DATAS } from '../../shared/constants';
+import OpeningHoursSchema from '../../models/opening_hours_schema';
+
+interface IOpen {
+	isOpen: boolean;
+	start: number;
+	end: number;
+}
+
+function isOpenNow(daysOpening: any): boolean {
+	const hoje = moment();
+
+	const diaSemana = DE_PARA_DATAS[hoje.weekday()];
+
+	console.log('dia Semana', diaSemana);
+
+	const day: IOpen = daysOpening[`${diaSemana}`];
+
+	if (!day.isOpen) {
+		return false;
+	}
+
+	const timeOpening = new Date().setHours(day.start, 0, 0, 0);
+	const timeClose = new Date().setHours(day.end, 0, 0, 0);
+
+	if (hoje.isSameOrAfter(timeOpening) && hoje.isBefore(timeClose)) {
+		return true;
+	}
+
+	return false;
+}
+
+async function areServicesOpen(list: ServiceInterface[]): Promise<void> {
+	const services = [];
+
+	for (let i = 0; i < list.length; i++) {
+		const openingHours = await OpeningHoursSchema.findOne({
+			_id: list[i].openingHour,
+		});
+
+		console.log(openingHours);
+
+		list[i].isOpen = isOpenNow(openingHours);
+	}
+}
+
+export default areServicesOpen;
